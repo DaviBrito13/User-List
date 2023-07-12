ec02ae2559a7526831b208d12ff7047de8479881
DaviBrito13 <davipires52@gmail.com>
2023-07-11 23:29:22 -0300

    User list consuming api

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,16 +1,11 @@
 class UsersController < ApplicationController
-  #Classe necessária para fazer requisições http
   require 'net/http'
-  #Classe necessária para manipular JSON
   require 'json'
-  #Classe para fazer a paginação
   require 'will_paginate/array'
 
   def index
     url = URI("https://run.mocky.io/v3/ce47ee53-6531-4821-a6f6-71a188eaaee0")
-    #Create an instance from Net::HTTP class
     http = Net::HTTP.new(url.host, url.port)
-    #Set a encrypted connection with the api
     http.use_ssl = true
 
     request = Net::HTTP::Get.new(url)
